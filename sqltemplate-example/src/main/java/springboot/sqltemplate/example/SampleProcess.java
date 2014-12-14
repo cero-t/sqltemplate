dd08ca748c0c2793b8754594da59e53f15292995
Shin Tanimoto <cel@pos.to>
2014-12-14 14:59:18 +0900

    remove unnecessary codes

--- sqltemplate-example/src/main/java/springboot/sqltemplate/example/SampleProcess.java
+++ sqltemplate-example/src/main/java/springboot/sqltemplate/example/SampleProcess.java
@@ -1,12 +1,9 @@
 package springboot.sqltemplate.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.util.ReflectionUtils;
 import springboot.sqltemplate.example.entity.Emp;
 
-import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,19 +11,9 @@ import java.util.Map;
 @Component
 public class SampleProcess {
     @Autowired
-    JdbcTemplate jdbcTemplate;
-
-    @Autowired
     SqlTemplate query;
 
     public void process() {
-        ReflectionUtils.doWithFields(Emp.class, new ReflectionUtils.FieldCallback() {
-            @Override
-            public void doWith(Field field) throws IllegalArgumentException, IllegalAccessException {
-                System.out.println(field);
-            }
-        });
-
         List<Emp> emps = query.forList("sql/selectAll.sql", Emp.class);
         emps.forEach(e -> System.out.println(e.ename)); // SMITH ... MILLER
 
